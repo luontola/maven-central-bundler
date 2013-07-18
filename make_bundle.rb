3bae8946f2ad1e000a4816ce5220ac08ba3385b3
Esko Luontola <esko.luontola@gmail.com>
2013-07-19 00:46:23 +0300

    Added copyright headers

--- make_bundle.rb
+++ make_bundle.rb
@@ -1,9 +1,17 @@
 #!/usr/local/bin/ruby
+# Copyright © 2011-2013, Esko Luontola <www.orfjackal.net>
+# This software is released under the Apache License 2.0.
+# The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
 require File.dirname(__FILE__) + '/lib/bundler'
 
 if ARGV.length == 0
+  puts 'Maven Central Bundler, https://github.com/orfjackal/maven-central-bundler'
   puts "Usage: #{File.basename(__FILE__)} --pom FILE --jar FILE --sources FILE [--CLASSIFIER FILE]..."
+  puts
+  puts 'Copyright (c) 2011-2013, Esko Luontola <www.orfjackal.net>'
+  puts 'This software is released under the Apache License 2.0.'
+  puts 'The license text is at http://www.apache.org/licenses/LICENSE-2.0'
   exit 1
 end
 
